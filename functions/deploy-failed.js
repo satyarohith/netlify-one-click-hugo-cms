a53b47ab39e64008115d25ec5a44c34840c7a70c
Satya Rohith <me@satyarohith.com>
2020-07-21 19:16:55 +0530

    test netlify triggers

--- functions/deploy-failed.js
+++ functions/deploy-failed.js
@@ -1,14 +1,5 @@
 exports.handler = function (event, context, callback) {
-  const sgMail = require('@sendgrid/mail');
-  sgMail.setApiKey(process.env.SENDGRID_API_TOKEN);
-  const msg = {
-    to: 'me@satyarohith.com',
-    from: 'test@example.com',
-    subject: 'Deploy failed triggered',
-    text: 'and easy to do anywhere, even with Node.js',
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>'
-  };
-  sgMail.send(msg);
+  console.log('deploy-failed called');
   callback(null, {
     statusCode: 200,
     body: {event, context}
